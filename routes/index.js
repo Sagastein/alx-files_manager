5948c95951708b0000de5d89c4ef8f4785508ef0
Ishimwe Sage <72102004+Sagastein@users.noreply.github.com>
2023-12-04 18:43:56 +0200

    "refactoring import and exports in the index file"

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,14 @@
-import { Router } from 'express';
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-
+import { Router } from "express";
+import AppController from "../controllers/AppController";
+import UsersController from "../controllers/UsersController";
+import AuthController from "../controllers/AuthController";
+//routes
 const router = Router();
+router.get("/status", AppController.getStatus);
+router.get("/stats", AppController.getStats);
+router.post("/users", UsersController.postNew);
+router.get("/connect", AuthController.getConnect);
+router.get("/disconnect", AuthController.getDisconnect);
+router.get("/users/me", UsersController.getMe);
 
-// status and stat route
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-
-// users route
-router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
-
-// authenticate route
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-
-export default router;
+module.exports = router;
