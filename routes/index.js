ac82c34bc921309ee2237bc8b6e5e004a5bb83ab
Sagastein <ishimwe.sage.3@gmail.com>
2023-12-04 07:34:49 -0800

    ft-users adding user controller and routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-//import AuthController from '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
@@ -14,7 +14,7 @@ router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 
 // authenticate route
-//router.get('/connect', AuthController.getConnect);
-//router.get('/disconnect', AuthController.getDisconnect);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
 export default router;
