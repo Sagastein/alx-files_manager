a96a752ce309ae134b9a225cf3c7f242f20fcd10
Sagastein <ishimwe.sage.3@gmail.com>
2023-12-04 06:51:15 -0800

    new(ft-controller) adding users controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-//import UsersController from '../controllers/UsersController';
+import UsersController from '../controllers/UsersController';
 //import AuthController from '../controllers/AuthController';
 
 const router = Router();
@@ -10,8 +10,8 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
 // users route
-//router.post('/users', UsersController.postNew);
-//router.get('/users/me', UsersController.getMe);
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
 
 // authenticate route
 //router.get('/connect', AuthController.getConnect);
